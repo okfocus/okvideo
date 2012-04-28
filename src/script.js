4c056a5a0504a02d16f5a6a346a7a18e1c681b3d
Jonathan Vingiano <jvingiano@gmail.com>
2012-04-27 20:14:15 -0400

    add src dir

---
+++ src/script.js
@@ -0,0 +1,181 @@
+/*
+ * OKVideo by OKFocus v1.0.0
+ * http://okfoc.us 
+ *
+ * Copyright 2012, OKFocus
+ * Licensed under the MIT license.
+ *
+ */
+
+var player, OKEvents;
+
+(function ($) {
+    
+    $.okvideo = function (options) {
+        var base = this;
+
+        base.init = function () {
+            base.options = $.extend({}, $.okvideo.options, options);
+            
+            $('body').append('<div style="position:fixed;left:0;top:0;overflow:hidden;z-index:-998;height:100%;width:100%;"></div><div id="player" style="position:fixed;left:0;top:0;overflow:hidden;z-index:-999;height:100%;width:100%;"></div>');
+            
+            base.setOptions();
+
+            if (base.options.source.provider === 'youtube') {
+                base.loadYoutubeAPI();
+            } else if (base.options.source.provider === 'vimeo') {
+                base.loadVimeoAPI();
+            }
+        };
+        
+        base.setOptions = function () {
+            base.options.source = base.determineProvider();
+            $(window).data('okoptions', base.options);
+        };
+
+        base.loadYoutubeAPI = function () {
+            base.insertJS('http://www.youtube.com/player_api');
+        };
+
+        base.loadVimeoAPI = function() {
+            $('#player').replaceWith(function() {
+                return '</div><iframe src="http://player.vimeo.com/video/' + base.options.source.id + '?api=1&js_api=1&title=0&byline=0&portrait=0&playbar=0&autoplay=1&loop=' + base.options.loop + '&player_id=player" frameborder="0" style="' + $(this).attr('style') + '" id="' + $(this).attr('id') + '"></iframe>';
+            });
+
+            base.insertJS('http://a.vimeocdn.com/js/froogaloop2.min.js', function(){ 
+                vimeoPlayerReady() 
+            });
+        };
+
+        base.insertJS = function(src, callback){
+            var tag = document.createElement('script');
+
+            if (callback){
+                if (tag.readyState){  //IE
+                    tag.onreadystatechange = function(){
+                        if (tag.readyState == "loaded" ||
+                            tag.readyState == "complete"){
+                            tag.onreadystatechange = null;
+                            callback();
+                        }
+                    };
+                } else {
+                    tag.onload = function() {
+                        callback();
+                    }
+                }
+            }
+            tag.src = src;
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        };
+
+        base.determineProvider = function () {
+            if (/youtube.com/.test(base.options.source) || /vimeo.com/.test(base.options.source)) {
+                return base.parseVideoURL(base.options.source);
+            } else if (/[A-Za-z0-9_]+/.test(base.options.source)) {
+                var id = base.options.source.match(/[A-Za-z0-9_]+/);
+                if (id[0].length != 11) throw "not youtube but thought it was for a sec";
+                return { 'provider' : 'youtube', 'id' : id };
+            } else {
+                for (var i = 0; i < base.options.source.length; i++) {
+                    if (typeof base.options.source[i] != 'number') {
+                        throw 'not vimeo but thought it was for a sec'
+                        break;
+                    }
+                }
+                return { 'provider' : 'vimeo', 'id' : base.options.source };
+            }
+        };
+
+        base.parseVideoURL = function(url) {
+
+            var retVal = {};
+            var matches;
+    
+            function getParm(url, param) {
+                var re = new RegExp("(\\?|&)" + param + "\\=([^&]*)(&|$)");
+                var matches = url.match(re);
+                if (matches) {
+                    return(matches[2]);
+                } else {
+                    return("");
+                }
+            }
+            
+            if (url.indexOf("youtube.com/watch") != -1) {
+                retVal.provider = "youtube";
+                retVal.id = getParm(url, "v");
+            } else if (matches = url.match(/vimeo.com\/(\d+)/)) {
+                retVal.provider = "vimeo";
+                retVal.id = matches[1];
+            }
+            return(retVal);
+        };
+
+        base.init();
+    };
+
+    $.okvideo.options = {
+        source: null,
+        disableKeyControl: 1,
+        captions: 0,
+        loop: 1
+    };
+
+    $.fn.okvideo = function (options) {
+        return this.each(function () {
+            (new $.okvideo(options));
+        });
+    };
+
+})(jQuery);
+
+function vimeoPlayerReady() {   
+
+    var vimeoPlayers = document.querySelectorAll('iframe');
+    for (var i = 0, length = vimeoPlayers.length; i < length; i++) {        
+        $f(vimeoPlayers[i]).addEvent('ready', OKEvents.v.ready);
+    }
+}
+
+function onYouTubePlayerAPIReady() {
+    var options = $(window).data('okoptions');
+    player = new YT.Player('player', {
+        videoId: options.source.id,
+        playerVars: {
+            'autohide': 1,
+            'autoplay': 1,
+            'disablekb': options.keyControls,
+            'cc_load_policy': options.captions,
+            'controls': 0,
+            'enablejsapi': 1,
+            'fs': 0,
+            'iv_load_policy': 1,
+            'loop': options.loop,
+            'showinfo': 0,
+            'rel': 0,            
+            'wmode': 'opaque'
+        },
+        events: {
+            'onReady': OKEvents.yt.ready,
+            'onError': OKEvents.yt.error
+        }
+    });
+}
+
+OKEvents = {
+    yt: {
+        ready: function(event){
+            event.target.mute();
+        },
+        error: function(event){
+            throw event;
+        }
+    },
+    v: {
+        ready: function(player_id){
+            $f(player_id).api('api_setVolume', 0);
+        }
+    }
+};
